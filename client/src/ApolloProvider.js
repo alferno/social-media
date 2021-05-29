547ff4f56ae00dd11b54f57fca8724dac3f5d271
Krishan K Yadav <aniandp@gmail.com>
2021-05-30 01:03:59 +0530

    server up

--- client/src/ApolloProvider.js
+++ client/src/ApolloProvider.js
@@ -5,7 +5,7 @@ import { ApolloProvider } from '@apollo/client/react'
 import { setContext } from 'apollo-link-context'
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:5000',
+  uri: 'https://graphql-social-media.herokuapp.com/',
 })
 
 const authLink = setContext(() => {
